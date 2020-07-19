58d7ca4419144a4f75fc190d1673431e99ac8ba9
julian sodo <sodojulian@gmail.com>
2020-07-19 14:22:47 -0300

    Cambios

--- src/configBd.js
+++ src/configBd.js
@@ -1,6 +1,5 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('pruebatesting', 'pruebatest', '123123jJ', {
-    host: 'db4free.net',
+const sequelize = new Sequelize('mysql://bd97c3cce1bd53:3e43baa3@us-cdbr-east-05.cleardb.net/heroku_c549a86d0e2b51f', {
     dialect: 'mysql',
     dialectOptions: {
       timeout: 42
